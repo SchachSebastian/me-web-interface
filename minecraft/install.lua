a55da69e51351f217c443025c8c662e0f695c500
Sebastian <schachner.contact@gmail.com>
2025-04-15 18:33:40 +0200

    added missing craftingHandler in install script

--- minecraft/install.lua
+++ minecraft/install.lua
@@ -2,13 +2,15 @@ shell.run(
     "wget https://raw.githubusercontent.com/SchachSebastian/me-web-interface/refs/heads/main/minecraft/meWebInterface/wsHandler.lua",
     "meWebInterface/wsHandler.lua")
 shell.run(
+    "wget https://raw.githubusercontent.com/SchachSebastian/me-web-interface/refs/heads/main/minecraft/meWebInterface/craftingHandler.lua",
+    "meWebInterface/craftingHandler.lua")
+
+shell.run(
     "wget https://raw.githubusercontent.com/SchachSebastian/me-web-interface/refs/heads/main/minecraft/meWebInterface/getMeInventory.lua",
     "meWebInterface/getMeInventory.lua")
 shell.run(
     "wget https://raw.githubusercontent.com/SchachSebastian/me-web-interface/refs/heads/main/minecraft/meWebInterface/getMeStorage.lua",
     "meWebInterface/getMeStorage.lua")
-
-
 
 local configSuccess = shell.run(
     "wget https://raw.githubusercontent.com/SchachSebastian/me-web-interface/refs/heads/main/minecraft/meWebInterface/config.lua",
