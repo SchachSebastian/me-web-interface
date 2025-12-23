1d15593477103de54239d07f75d062d8fc150b9a
Sebastian <schachner.contact@gmail.com>
2025-12-23 17:51:51 +0100

    fix(): inventory update fixes

--- minecraft/webInterface/inventory.lua
+++ minecraft/webInterface/inventory.lua
@@ -18,6 +18,54 @@ local function cleanDisplayName(displayName)
     return displayName
 end
 
+local function parseItem(item)
+    local components = nil
+    if (next(item.components)) then
+        components = item.components
+    end
+
+    local new_item = {
+        id = item.fingerprint .. 'i',
+        name = item.name,
+        count = item.count,
+        displayName = cleanDisplayName(item.displayName),
+        components = components,
+        isCraftable = item.isCraftable
+    }
+    return new_item
+end
+
+local function parseFluid(fluid)
+    local components = nil
+    if (next(fluid.components)) then
+        components = fluid.components
+    end
+
+    local new_fluid = {
+        id = fluid.fingerprint .. 'f',
+        name = fluid.name,
+        count = fluid.count / 1000,
+        displayName = cleanDisplayName(fluid.displayName),
+        components = components,
+        isCraftable = fluid.isCraftable,
+        isFluid = true
+    }
+    return new_fluid
+end
+
+local function parseChemical(chemical)
+    local new_chemical = {
+        id = chemical.fingerprint .. 'c',
+        name = chemical.name,
+        count = chemical.count / 1000,
+        displayName = cleanDisplayName(chemical.displayName),
+        isCraftable = false,
+        isChemical = true
+    }
+
+    return new_chemical
+end
+
 local function getNewInventory()
     local inventory = {}
 
@@ -25,70 +73,35 @@ local function getNewInventory()
     local craftableItems = bridge.getCraftableItems()
     for _, craftableItem in ipairs(craftableItems) do
         if craftableItem.count == 0 then
-            table.insert(items, craftableItem)
+            table.insert(items, parseItem(craftableItem))
         end
     end
 
     for _, item in ipairs(items) do
-        local components = nil
-        if (next(item.components)) then
-            components = item.components
-        end
-
-        local new_item = {
-            id = item.fingerprint + 'i',
-            name = item.name,
-            count = item.count,
-            displayName = cleanDisplayName(item.displayName),
-            components = components,
-            isCraftable = item.isCraftable
-        }
-        table.insert(inventory, new_item)
+        table.insert(inventory, parseItem(item))
     end
 
     local fluids = bridge.getFluids()
     local craftableFluids = bridge.getCraftableFluids()
     for _, craftableFluid in ipairs(craftableFluids) do
         if craftableFluid.count == 0 then
-            table.insert(fluids, craftableFluid)
+            table.insert(fluids, parseFluid(craftableFluid))
         end
     end
     for _, fluid in ipairs(fluids) do
-        local components = nil
-        if (next(fluid.components)) then
-            components = fluid.components
-        end
-
-        local new_fluid = {
-            id = fluid.fingerprint + 'f',
-            name = fluid.name,
-            count = fluid.count/1000,
-            displayName = cleanDisplayName(fluid.displayName),
-            components = components,
-            isCraftable = fluid.isCraftable,
-            isFluid = true
-        }
-        table.insert(inventory, new_fluid)
+        table.insert(inventory, parseFluid(fluid))
     end
 
     local chemicals = bridge.getChemicals()
     local craftableChemicals = bridge.getCraftableChemicals()
     for _, craftableChemical in ipairs(craftableChemicals) do
         if craftableChemical.count == 0 then
-            table.insert(chemicals, craftableChemical)
+            table.insert(chemicals, parseChemical(craftableChemical))
         end
     end
 
     for _, chemical in ipairs(chemicals) do
-        local new_gas = {
-            id = chemical.fingerprint + 'c',
-            name = chemical.name,
-            count = chemical.count/1000,
-            displayName = cleanDisplayName(chemical.displayName),
-            isCraftable = false,
-            isChemical = true
-        }
-        table.insert(inventory, new_gas)
+        table.insert(inventory, parseChemical(chemical))
     end
 
     return inventory
@@ -98,34 +111,31 @@ local function getInventoryDiff()
     if (bridge == nil or not bridge.isConnected() or not bridge.isOnline()) then
         return {}
     end
-    
+
     local items = getNewInventory()
     local list = {}
     local seenItems = {}
 
     for _, item in ipairs(items) do
-        local item_key = item.id + (tostring(item.isChemical) .. tostring(item.isFluid))
-        local saved_item = storage[item_key] or {}
+        local saved_item = storage[item.id] or {}
 
         local diff = getDiff(saved_item, item)
         if next(diff) then
             diff.id = item.id
             table.insert(list, diff)
         end
-        storage[item_key] = item
-        seenItems[item_key] = true
+        storage[item.id] = item
+        seenItems[item.id] = true
     end
 
     for _, item in pairs(storage) do
-        local item_key = item.id + (tostring(item.isChemical) .. tostring(item.isFluid))
-
-        if not seenItems[item_key] then
+        if not seenItems[item.id] then
 
             table.insert(list, {
                 id = item.id,
                 count = -1
             })
-            storage[item_key] = nil
+            storage[item.id] = nil
         end
     end
     return list
@@ -137,5 +147,5 @@ end
 
 return {
     getInventoryDiff = getInventoryDiff,
-    resetStorage = resetStorage,
+    resetStorage = resetStorage
 };
